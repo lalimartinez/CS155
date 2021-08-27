79df54e2575cc55a58c9565df8829126e1299bb2
Zitlali Ibarra <W7266249@student.miracosta.edu>
2021-08-27 11:57:08 -0700

    Add project files.

---
+++ Homework1Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework1Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         /\\");
+            Console.WriteLine("        /  \\");
+            Console.WriteLine("       /    \\");
+            Console.WriteLine("      /      \\");
+            Console.WriteLine("     /        \\");
+            Console.WriteLine("    /          \\");
+            Console.WriteLine("   /            \\");
+            Console.WriteLine("  /              \\");
+            Console.WriteLine(" /                \\");
+            Console.WriteLine("/__________________\\");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|                   |");
+            Console.WriteLine("|      _______      |");
+            Console.WriteLine("|      |     |      |");
+            Console.WriteLine("|      |     |      |");
+            Console.WriteLine("|      |     |      |");
+            Console.WriteLine("|______|_____|______|");
+        }
+    }
+}
