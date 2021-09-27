f56fe90690994604f8afa1589b960161dcdd8c6b
lalimartinez <martinezlali4@gmail.com>
2021-09-26 18:49:38 -0700

    Added HomeworkWeek5

---
+++ HomeworkWeek5Part1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿/// Chapter No. 4		Exercise No. 1
+/// File Name: BMICalculatorGUI.cs
+/// @author: Zitlali Martinez
+/// Date:  September 26, 2021
+///
+/// Problem Statement: Ask user to enter their height an weight. Calculate their BMI. 
+/// Display a message saying if their BMI is below, within, or above the healthy range and change the background color accordingly.
+/// 
+/// Overall Plan:
+/// 1) Prompt user for their weight in pounds 
+/// 2) Prompt user for their height in inches
+/// 3) Prompt user to click the button 
+/// 4) Calculate the user's BMI
+/// 5) Print message saying whether BMI is below, within, or above the healthy range
+/// 6) If BMI is below healthy range, change background color to blue
+/// 7) Else if, BMI is within healthy range, change background color to green
+/// 8) Else, change background color to yellow
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HomeworkWeek5Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Read in weight and heigh from textboxes
+            double weight = double.Parse(weightTextBox.Text);
+            double height = double.Parse(heightTextBox.Text);
+            double BMI;
+
+            //Calculate BMI
+            BMI = (weight * 720) / (height * height);
+
+            //Set text in BMI textbox to the BMI
+            BMItextBox.Text = BMI.ToString();
+
+            //Use if/else if/else to change color 
+            //Blue if below helthy range, green if healthy, yellow if above
+            if(BMI < 15)
+            {
+                TempCanvas.Background = Brushes.LightBlue;
+                resultsLabel.Content = "Your BMI is below the healthy range.";
+            }
+            else if(BMI >= 15 && BMI <= 26)
+            {
+                TempCanvas.Background = Brushes.Green;
+                resultsLabel.Content = "Your BMI is within the healthy range.";
+            }
+            else
+            {
+                TempCanvas.Background = Brushes.Yellow;
+                resultsLabel.Content = "Your BMI is above the healthy range.";
+            }
+        }
+    }
+}
---
+++ HomeworkWeek5Part2/Program.cs
@@ -0,0 +1,58 @@
+﻿/// Chapter No. 4		Exercise No. 2
+/// File Name: CoinToss.cs
+/// @author: Zitlali Martinez
+/// Date:  September 26, 2021
+///
+/// Problem Statement: Ask user to enter heads or tails for each coin toss. 
+/// Display the total number and percentages of heads and tails tossed.
+/// 
+/// Overall Plan:
+/// 1) Prompt user to enter to enter 'h' for heads or 't' for tails 
+/// 2) Use a for loop to ask user for heads or tails eight times
+/// 3) If user enters 'h', increase the number of heads, else increase the number of tails
+/// 4) Calculate the percentage of heads and tails
+/// 5) Print how many heads and tails there were and the percentage of each to the screen
+/// 
+using System;
+
+namespace HomeworkWeek5Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create array to hold number of toss
+            string[] toss = { "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth" };
+            //Create variables to hold counts
+            int headCount = 0, tailCount = 0;
+            string headsOrTails;
+            double headPercent, tailPercent;
+
+            Console.WriteLine("For each coin toss enter 'h' for heads or 't' for tails.");
+
+            //Use for loop to go through coin tosses
+            for(int i = 0; i < 8; i++)
+            {
+                Console.Write(toss[i] + " toss: ");
+                headsOrTails = Console.ReadLine();
+                //If h increase head count, else increase tail count
+                if (headsOrTails == "h")
+                    headCount++;
+                else
+                    tailCount++;
+            }
+
+            //Calculate percentages
+            headPercent = ((double)headCount / 8) * 100;
+            tailPercent = ((double)tailCount / 8) * 100;
+
+            //Display results
+            Console.WriteLine("Number of heads: " + headCount);
+            Console.WriteLine("Number of tails: " + tailCount);
+            Console.WriteLine("Percent heads: " + headPercent);
+            Console.WriteLine("Percent tails: " + tailPercent);
+
+            Console.ReadLine();
+        }
+    }
+}
