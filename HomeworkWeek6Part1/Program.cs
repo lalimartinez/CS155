59ab2984aeadc447d6ee6259c7ae3ba207988833
lalimartinez <martinezlali4@gmail.com>
2021-10-03 19:07:32 -0700

    Added HomeworkWeek6

---
+++ HomeworkWeek6Part1/Program.cs
@@ -0,0 +1,47 @@
+﻿/// Chapter No. 5		Exercise No. 1
+/// File Name: CopyArray.cs
+/// @author: Zitlali Martinez
+/// Date:  October 03, 2021
+///
+/// Problem Statement: Fill an array with ten numbers. Then create a second array and copy the 
+/// values of the first array into the second array. Print both arrays to the screen.
+/// 
+/// Overall Plan:
+/// 1) Create an array to hold ten values and fill the array 
+/// 2) Create a second array the same length of the first
+/// 3) Use a for loop to fill the second array with the same values as the first array
+/// 4) Print both arrays to the screen
+/// 
+using System;
+
+namespace HomeworkWeek6Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create first array
+            int[] numbers = new int[10];
+            //Fill first array
+            for (int i = 0; i < 10; i++)
+                numbers[i] = i * 5;
+
+            //Create copy array
+            int[] copyArray = new int[numbers.Length];
+            //Fill copy array with numbers in first array
+            for (int i = 0; i < numbers.Length; i++)
+                copyArray[i] = numbers[i];
+
+            //Print both arrays to the scree
+            Console.WriteLine("First Array:");
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write(numbers[i] + " ");
+            Console.WriteLine("\nCopy Array:");
+            for (int i = 0; i < copyArray.Length; i++)
+                Console.Write(copyArray[i] + " ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HomeworkWeek6Part2/Program.cs
@@ -0,0 +1,67 @@
+﻿/// Chapter No. 5		Exercise No. 2
+/// File Name: ReverseArray.cs
+/// @author: Zitlali Martinez
+/// Date:  October 03, 2021
+///
+/// Problem Statement: Create a method to generate an array, a method to print an array, and 
+/// a method to reverse the array. Use the methods to reverse an array and print it.
+/// 
+/// Overall Plan:
+/// 1) Create a method that returns an array filled with ten values 
+/// 2) Create a method that takes in an array and prints it to the screen
+/// 3) Create a method that takes in an array, reverses it, and returns the reversed array
+/// 4) Create an array and use the method that generates an array to fill it
+/// 5) Send the array into the reverse method
+/// 6) Send the array into the method that prints it to the screen
+/// 
+using System;
+
+namespace HomeworkWeek6Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = GenerateNumbers();
+            Reverse(numbers);
+            PrintNumbers(numbers);
+
+            Console.ReadLine();
+        }
+
+        //Method to generate an array of numbers
+        static int[] GenerateNumbers()
+        {
+            //Declare array
+            int[] nums = new int[10];
+            //Fill array
+            for (int i = 0; i < 10; i++)
+                nums[i] = i;
+            //Return array
+            return nums;
+        }
+
+        //Method to print array of numbers
+        static void PrintNumbers(int[] nums)
+        {
+            foreach (int element in nums)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+
+        //Method to revers the array
+        static int[] Reverse(int[] nums)
+        {
+            int temp;
+            //Use a loop to go through half the array and swap values starting with the first and last to reverse it
+            for(int i = 0; i < nums.Length/2; i++)
+            {
+                temp = nums[i];
+                nums[i] = nums[nums.Length - i - 1];
+                nums[nums.Length - i - 1] = temp;
+            }
+            //Return array
+            return nums;
+        }
+    }
+}
