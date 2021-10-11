e8af21ae4c795877638694801336e0490aad3e49
lalimartinez <martinezlali4@gmail.com>
2021-10-10 19:55:32 -0700

    Added HomeworkWeek7

---
+++ HomeworkWeek7Part1/Counter.cs
@@ -0,0 +1,79 @@
+﻿/// Chapter No. 6		Exercise No. 1
+/// File Name: Counter.cs
+/// @author: Zitlali Martinez
+/// Date:  October 10, 2021
+///
+/// Problem Statement: Write a counter class that creates a counter object to count things.
+/// 
+/// Overall Plan:
+/// 1) Write method to set counter to zero
+/// 2) Write method to increase count by one
+/// 3) Write method to decrease count by one as long as the count is greater than zero
+/// 4) Write a getter method that return the count
+/// 5) Write method that outputs the current count of the object
+/// 6) Override the ToString method to display the counter object
+/// 7) Override the Equals method to compare two counter objects
+/// 
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkWeek7Part1
+{
+    class Counter
+    {
+        private int count;
+
+        //Set counter to 0
+        public void setCount()
+        {
+            count = 0;
+        }
+
+        //Increase count by one
+        public void increaseCount()
+        {
+            count++;
+        }
+
+        //Decrease count by one
+        public void decreaseCount()
+        {
+            //Only decrease count if it is greater than 0
+            if (count > 0)
+                count--;
+            else
+                Console.WriteLine("Counter cannot go negative.");
+        }
+
+        //Getter method to get the count
+        public int getCount()
+        {
+            return count;
+        }
+
+        //Method to output count to string
+        public void currentCount()
+        {
+            Console.WriteLine("The current count is " + count);
+        }
+
+        //ToString method
+        public override string ToString()
+        {
+            return "Count: " + count;
+        }
+
+        //Equals method
+        public override bool Equals(Object other)
+        {
+            if (other == null)
+                return false;
+            if (!(other is Counter))
+                return false;
+
+            Counter o = (Counter)other;
+            return this.count == o.count;
+        }
+    }
+}
---
+++ HomeworkWeek7Part1/Program.cs
@@ -0,0 +1,101 @@
+﻿/// Chapter No. 6		Exercise No. 1
+/// File Name: CounterDriver.cs
+/// @author: Zitlali Martinez
+/// Date:  October 10, 2021
+///
+/// Problem Statement: Write a driver program with a menu to test each method of the Counter class.
+/// 
+/// Overall Plan:
+/// 1) Create two counter objects 
+/// 2) Display menu of choices for one counter object and prompt user for a choice
+/// 3) Use a switch statement to switch between choices
+/// 4) For choice 1 increase the count
+/// 5) For choice 2 decrease the count
+/// 6) For choice 3 display the current count
+/// 7) For choice 4 reset count to zero
+/// 8) For choice 5 break out of the menu loop
+/// 9) As a default, tell user their choice must be between 1-5
+/// 10) While choice isn't 5, keep displaying menu
+/// 11) Display the final count of the first counter object and the count of the second object
+/// 12) Compare the two counter objects to see if they are the same
+/// 
+using System;
+
+namespace HomeworkWeek7Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            //Create Counter objects and set to zero
+            Counter count1 = new Counter();
+            count1.setCount();
+            Counter count2 = new Counter();
+            count2.setCount();
+
+
+            do
+            {
+                //Create menu
+                Console.WriteLine("\nChoose one of the following." +
+                    "\n1) Increase count by 1." +
+                    "\n2) Decrease count by 1." +
+                    "\n3) Display current count." +
+                    "\n4) Reset counter to zero." +
+                    "\n5) Exit." +
+                    "\nEnter choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    //Increase count by 1
+                    case 1:
+                        count1.increaseCount();
+                        break;
+
+                    //Decrease count by 1
+                    case 2:
+                        count1.decreaseCount();
+                        break;
+
+                    //Display current count
+                    case 3:
+                        Console.WriteLine();
+                        count1.currentCount();
+                        break;
+
+                    //Reset count to zero
+                    case 4:
+                        count1.setCount();
+                        break;
+
+                    //Exit
+                    case 5:
+                        Console.WriteLine("\nExiting.");
+                        break;
+
+                    //If choice enters is not 1-5
+                    default:
+                        Console.WriteLine("\nChoice must be between 1-5");
+                        break;
+                }
+            } while (choice != 5);
+
+            //Display final count of count1 using toString method
+            Console.WriteLine("\nThe final count of your counter.\n" + count1.ToString());
+
+            //Display count of second count object
+            Console.WriteLine("The count of the second counter is " + count2.getCount());
+
+            //Compare counters using equals method
+            Console.WriteLine("\nAre the two counters the same?");
+            if (count1.Equals(count2))
+                Console.WriteLine("The counters are the same.");
+            else
+                Console.WriteLine("The counters are not the same.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
