12e81b51c76e15466ffbb1f805f47d07857e3f0b
lalimartinez <martinezlali4@gmail.com>
2021-10-25 12:18:34 -0700

    Added Lab9

---
+++ Week9Lab/Pizza.cs
@@ -0,0 +1,74 @@
+﻿/// Chapter No. 7		Lab No. 1
+/// File Name: Pizza.cs
+/// @author: Zitlali Martinez
+/// Date:  October 25, 2021
+///
+/// Problem Statement: Write a Pizza class that stores the information for one pizza.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Write two constructors: a no argument one and one for all the instance variables
+/// 2) Write the getters and setters using properties
+/// 3) Write a method to calculate the cost of the pizza
+/// 4) Override the toString method to display the pizza information
+///
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Week9Lab
+{
+    class Pizza
+    {
+        private string size;
+        private int cheese;
+        private int pepperoni;
+        private int ham;
+
+        //Empty constructor
+        public Pizza() { }
+
+        //Overloaded constructor 
+        public Pizza(string _size, int _cheese, int _pepperoni, int _ham)
+        {
+            size = _size;
+            cheese = _cheese;
+            pepperoni = _pepperoni;
+            ham = _ham;
+        }
+
+        //Getters and Setters
+        public string Size { get => size; set => size = value; }
+        public int Cheese { get => cheese; set => cheese = value; }
+        public int Pepperoni { get => pepperoni; set => pepperoni = value; }
+        public int Ham { get => ham; set => ham = value; }
+
+        //Method to calculate cost 
+        public double CalculateCost()
+        {
+            double cost = ((double)cheese * 2) + (pepperoni * 2) + (ham * 2);
+            //If small add 10 to the cost
+            if (size == "small")
+                cost += 10.0;
+            //Else if medium add 12 to the cost
+            else if (size == "medium")
+                cost += 12.0;
+            //Else add 14 to cost for a large
+            else
+                cost += 14.0;
+            return Math.Round(cost, 2);
+        }
+
+        //Overriden toString method 
+        public string toString()
+        {
+            return "Pizza size: " + size
+            + "\nCheese Toppings: " + cheese
+            + "\nPepperoni Toppings: " + pepperoni
+            + "\nHam Toppings: " + ham
+            + "\nCost: $" + CalculateCost();
+        }
+    }
+}
---
+++ Week9Lab/Program.cs
@@ -0,0 +1,48 @@
+﻿/// Chapter No. 7		Lab No. 1
+/// File Name: PizzaDriver.cs
+/// @author: Zitlali Martinez
+/// Date:  October 25, 2021
+///
+/// Problem Statement: Write a driver program to test the methods of the Pizza class.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create two pizza objects using both constructors
+/// 2) Use the setter to set the information for one of the pizzas
+/// 3) Use one the getter to get the size of the second pizza
+/// 4) Calculate the cost of one the pizzas using the CalculateCost method
+/// 5) Display the information of both pizzas using the toString method
+///
+
+using System;
+
+namespace Week9Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create pizzas using constructors
+            Pizza myPizza = new Pizza();
+            Pizza otherPizza = new Pizza("medium", 2, 3, 3);
+
+            //Use set on myPizza
+            myPizza.Size = "small";
+            myPizza.Cheese = 1;
+            myPizza.Pepperoni = 3;
+            myPizza.Ham = 2;
+
+            //Use get method on otherPizza 
+            Console.WriteLine("Size of other pizza: " + otherPizza.Size);
+
+            //Use calculate cost method 
+            Console.WriteLine("Cost of myPizza: $" + myPizza.CalculateCost());
+
+            //Use toString methods 
+            Console.WriteLine("\nMy Pizza\n" + myPizza.toString());
+            Console.WriteLine("\nOther Pizza\n" + otherPizza.toString());
+
+            Console.ReadLine();
+        }
+    }
+}
